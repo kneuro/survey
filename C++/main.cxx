20ae0a3a3be6e9a241d223704241797579e12cd2
Aphotica K. Lector <jake.m.knepper@gmail.com>
2017-09-12 15:05:44 -0500

    Added source for C++ program.

---
+++ C++/main.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int age;
+
+  cout << "\nWelcome! What is your age?\n\n";
+  cin >> age;
+
+  cout << "\nYou are " << age;
+  cout << " years old!\n\n";
+
+  return 0;
+}
