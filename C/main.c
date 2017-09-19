110984797b9fecfc2aa296917654194600be6d98
Aphotica K. Lector <jake.m.knepper@gmail.com>
2017-09-19 18:23:53 -0500

    Added name input and printing to an output text file in C.

--- C/main.c
+++ C/main.c
@@ -24,6 +24,29 @@ int main ()
     else
     {
       printf("\nThat's not an age! Try again...\n\n");
+      return 0;
     }
+  printf("Nice! Now, what is your name?\n");
+  printf("\nEnter your name: ");
+  char name[100];
+  scanf("%s", name);
+  printf("\nHello %s! It's nice to meet you!\n", name);
+  printf("\nWould you like to write your age and name to a text file?\n");
+  int yn;
+  printf("\nPress 1 for yes or 2 for no! ");
+  scanf("%d",&yn);
+  if (yn == 1) {
+    FILE *fp;
+    fp = fopen("Survey.txt", "w+");
+    fprintf(fp, "\nAge: %d", age);
+    fprintf(fp, "\nName: %s\n\n", name);
+    fclose(fp);
+    printf("\nAll done! Check Survey.txt in the current directory!\n\n");
+    return 0;
+  }
+  else {
+    printf("\nOkay! Exiting...\n\n");
+    return 0;
+  }
   return (0);
 }
